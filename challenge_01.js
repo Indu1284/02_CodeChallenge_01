6727b24ba09b7ec705ccf65aaefe97fced47339c
Indu <indurevathy@gmail.com>
2021-07-29 10:18:56 +0200

    added Indu code

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,12 @@ Berechnen Sie die Summe der Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
+
+let Value1;
+let Value2;
+let summ;
+
+Value1 = prompt("Please enter the first value:");
+Value2 = prompt("Please enter the second value:");
+summ = Number(Value1) + Number(Value2);
+console.log("The result is:" + summ);
